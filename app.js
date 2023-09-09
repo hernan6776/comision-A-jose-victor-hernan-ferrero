de6d82e903b410c6a179442df042d10e569c5ff6
Hernan <hernanferrero1@gmail.com>
2023-09-09 17:11:46 -0300

    Segunda subida al repositorio remoto del trabajo final

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-"hoja de prueba para trabajo final"
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.use('/',taskRouter)
+
+app.listen(port,() => {
+    console.log(`server listening http://localhost:${port}`)
+})
