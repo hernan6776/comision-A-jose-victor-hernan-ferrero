e3b95809772dcf55479c31820c535014c3114cf9
Hernan <hernanferrero1@gmail.com>
2023-09-10 20:41:52 -0300

    subida actualizacion prueba

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { ctrlCreateTask, ctrlDeleteTask, ctrlGetTasks, ctrlUpdateTask } from "../controllers/task.controllers.js";
 import { createTaskSchema } from "../models/schemas/task.schema.js";
 import { validator } from "../middlewares/validator.js";
+import { TaskModel } from "../models/Tasks.js";
 
 
 const taskRouter = Router();
