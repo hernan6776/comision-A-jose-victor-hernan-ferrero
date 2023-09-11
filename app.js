1f3b02528d45d93f386c3579bde0cdb92ab0712b
Hernan <hernanferrero1@gmail.com>
2023-09-11 16:02:17 -0300

    subida n°9 comienzo de creacion del front-end

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ import express from 'express';
 import { taskRouter } from './src/routes/task.routes.js';
 import { startDb } from './src/config/database.js';
 
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
-app.use(cors());
+// app.use(cors());
 
 //middlewares
 app.use(express.json())
