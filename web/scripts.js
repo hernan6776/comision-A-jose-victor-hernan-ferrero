bf83162a235eb318b2b3e57cd1bca7317a1d1dc9
Hernan <hernanferrero1@gmail.com>
2023-09-13 02:22:59 -0300

    subida repo n°13 agregado del delete e incorporacion de la libreria sweetalert

--- web/scripts.js
+++ web/scripts.js
@@ -20,6 +20,61 @@ btnCrear.addEventListener("click", () => {
     myModal.show()
 })
 
+document.addEventListener("click", (event) => {
+    if(event.target.matches("#btn-delete")){
+        const article = event.target.closest(".col-4")
+        const idArticle = article.dataset.id
+        
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:3000/api/tasks/${idArticle}`,{
+                    method: "DELETE"
+                }).then(res =>{
+                    if(res.ok){
+                        article.remove()
+                    }
+                }).catch(err =>{
+                    console.error(err)
+                })
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+            }
+        })
+
+    }
+})
+
+// document.addEventListener("click", (event) => {
+//     if(event.target.matches("#btn-delete")){
+//         const article = event.target.closest(".col-4")
+//         const idArticle = article.dataset.id
+//      
+
+//         fetch(`http://localhost:3000/api/tasks/${idArticle}`,{
+//             method: "DELETE"
+//         }).then(res =>{
+//             if(res.ok){
+//                 article.remove()
+//             }
+//         }).catch(err =>{
+//             console.error(err)
+//         })
+//     }
+// })
+
+
+
 fetch('http://localhost:3000/api/tasks')
     .then(res => res.json())
     .then(data => {
@@ -33,8 +88,8 @@ fetch('http://localhost:3000/api/tasks')
                         <h5 class="card-title">${task.title}</h5>
                         <p class="card-text">${task.description}</p>
                         <div>
-                            <button href="#" class="btn btn-success">Edit</button>
-                            <button href="#" class="btn btn-danger">Delete</button>
+                            <button class="btn btn-success" id="btn-edit">Edit</button>
+                            <button type="" class="btn btn-danger" id="btn-delete">Delete</button>
                         </div>
                     </div>
                 </div>
