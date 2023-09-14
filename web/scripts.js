086657e595e106d6adfb72535d43f435b9f16847
Hernan <hernanferrero1@gmail.com>
2023-09-14 01:02:23 -0300

    subida repo n°14 actualizacion de botones input, submit, new

--- web/scripts.js
+++ web/scripts.js
@@ -1,29 +1,30 @@
 
-const contenedor = document.getElementById("container-row")
-const btnCrear = document.getElementById("btn-new")
-const myModal = new bootstrap.Modal(document.getElementById('myModal'))
-const btnSave = document.getElementById("btn-save")
+const contenedor = document.getElementById("container-row");
+const btnCrear = document.getElementById("btn-new");
+const myModal = new bootstrap.Modal(document.getElementById('myModal'));
+const btnSave = document.getElementById("btn-save");
+const form = document.getElementById("formulario");
 
-let html = ''
-let option = ''
+let html = '';
+let option = '';
 
-const inputTitle = document.getElementById("inputTitle")
-const inputDescription = document.getElementById("inputDescription")
-const inputPoster = document.getElementById("inputPoster")
+const inputTitle = document.getElementById("inputTitle");
+const inputDescription = document.getElementById("inputDescription");
+const inputPoster = document.getElementById("inputPoster");
 
 btnCrear.addEventListener("click", () => {
     option = "new"
-    btnSave.textContent = "New"
-    inputTitle.value = ""
-    inputDescription.value = ""
-    inputPoster.value = ""
-    myModal.show()
-})
+    btnSave.textContent = "New";
+    inputTitle.value = "";
+    inputDescription.value = "";
+    inputPoster.value = "";
+    myModal.show();
+});
 
 document.addEventListener("click", (event) => {
     if(event.target.matches("#btn-delete")){
-        const article = event.target.closest(".col-4")
-        const idArticle = article.dataset.id
+        const article = event.target.closest(".col-4");
+        const idArticle = article.dataset.id;
         
         Swal.fire({
             title: 'Are you sure?',
@@ -73,12 +74,60 @@ document.addEventListener("click", (event) => {
 //     }
 // })
 
+document.addEventListener("click", (event) => {
+    if(event.target.matches("#btn-edit")){
+        const article = event.target.closest(".col-4");
 
+        const idArticle = article.dataset.id;
+        const urlPosterEdit = article.children[0].children[0].src;
+        const titleEdit = article.children[0].children[1].children[0].textContent;
+        const descriptionEdit = article.children[0].children[1].children[1].textContent;
+
+        inputTitle.value = titleEdit;
+        inputDescription.value = descriptionEdit;
+        inputPoster.value = urlPosterEdit;
+        option = "edit";
+        btnSave.textContent = "edit";
+        myModal.show();
+    }
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("Submit");
+
+    if(option === "new") {
+        const newTask = {
+            title:inputTitle.value,
+            description: inputDescription.value,
+            url:inputPoster.value,
+        };
+
+        fetch('http://localhost:3000/api/tasks', {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(newTask),
+        }).then(res => {
+            if (res.ok) {
+                alert('Posteo Created Successfully');
+                myModal.hide();
+                location.reload();
+            }
+        });
+    }   
+
+    // if(option === "edit") {
+
+    // }
+
+});
 
 fetch('http://localhost:3000/api/tasks')
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+        console.log(data);
         data.forEach(task => {
             html += `
             <article class="col-4 d-flex justify-content-center mb-3" data-id="${task.id}">
@@ -98,4 +147,4 @@ fetch('http://localhost:3000/api/tasks')
 
             contenedor.innerHTML = html;
         });
-    })
+    });
